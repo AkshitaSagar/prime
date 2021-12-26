55af42ea9be11bca5c9b50a5b3a4c0bad211e192
AkshitaSagar <94096782+AkshitaSagar@users.noreply.github.com>
2021-12-26 18:44:24 +0530

    Add files via upload

---
+++ prime.js
@@ -0,0 +1,16 @@
+//console.log("hoi");
+
+
+var n=prompt("enter number");
+{
+for(i=2;i<=n;i++)
+{
+if (n%i == 0)
+break;
+}
+}
+if(i==n)
+alert('prime number');
+else
+alert('not a prime number');
+
